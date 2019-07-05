c629c788db4de57dd2b03a66a2cce360b7478560
chenaihong <chenaihong@gcstgroup.com>
2019-07-05 18:25:31 +0800

    Revert "Revert "wip(product/*): 添加产品模块""
    This reverts commit 3815a1a71d9074e6e80be3a6b1269dee7261f414.

---
+++ src/app/routes/product/product-detail/product-detail.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.page.html',
+  styleUrls: ['./product-detail.page.scss'],
+})
+export class ProductDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/routes/product/product-directory/product-directory.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-directory',
+  templateUrl: './product-directory.page.html',
+  styleUrls: ['./product-directory.page.scss'],
+})
+export class ProductDirectoryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
