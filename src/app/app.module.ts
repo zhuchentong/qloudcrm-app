b857fda7ab0d4c34b9607f2d6398384d6aeff774
chenaihong <chenaihong@gcstgroup.com>
2019-07-05 18:14:32 +0800

    wip(product/*): 添加产品模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { RouteReuseStrategy } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular'
-import { SplashScreen } from '@ionic-native/splash-screen/ngx'
-import { StatusBar } from '@ionic-native/status-bar/ngx'
-
-import { AppComponent } from './app.component'
-import { RoutesModule } from './routes/routes.module'
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppComponent } from './app.component';
+import { RoutesModule } from './routes/routes.module';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/routes/product/product.module.ts
+++ src/app/routes/product/product.module.ts
@@ -1,22 +1,38 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
-import { Routes, RouterModule } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+// import { TreeModule } from 'ng2-tree';
+import { IonicModule } from '@ionic/angular';
 
-import { IonicModule } from '@ionic/angular'
-
-import { ProductPage } from './product.page'
-import { SharedModule } from 'src/app/shared/shared.module'
-
+import { ProductPage } from './product.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ProductDetailPage } from './product-detail/product-detail.page';
+import { ProductDirectoryPage } from './product-directory/product-directory.page';
 const routes: Routes = [
   {
     path: '',
     component: ProductPage
-  }
-]
+  },
+  {
+    path: 'product-detail',
+    component: ProductDetailPage
+  },
+  {
+    path: 'product-directory',
+    component: ProductDirectoryPage
+  },
+];
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild(routes)],
-  declarations: [ProductPage]
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes)],
+  declarations: [
+    ProductPage,
+    ProductDetailPage,
+    ProductDirectoryPage
+  ]
 })
-export class ProductPageModule {}
+export class ProductPageModule {
+}
