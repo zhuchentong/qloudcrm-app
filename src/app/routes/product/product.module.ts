3815a1a71d9074e6e80be3a6b1269dee7261f414
chenaihong <chenaihong@gcstgroup.com>
2019-07-05 18:22:29 +0800

    Revert "wip(product/*): 添加产品模块"
    This reverts commit b857fda7ab0d4c34b9607f2d6398384d6aeff774.

--- src/app/routes/product/product.module.ts
+++ src/app/routes/product/product.module.ts
@@ -1,38 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-// import { TreeModule } from 'ng2-tree';
-import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
+import { Routes, RouterModule } from '@angular/router'
 
-import { ProductPage } from './product.page';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { ProductDetailPage } from './product-detail/product-detail.page';
-import { ProductDirectoryPage } from './product-directory/product-directory.page';
+import { IonicModule } from '@ionic/angular'
+
+import { ProductPage } from './product.page'
+import { SharedModule } from 'src/app/shared/shared.module'
+
 const routes: Routes = [
   {
     path: '',
     component: ProductPage
-  },
-  {
-    path: 'product-detail',
-    component: ProductDetailPage
-  },
-  {
-    path: 'product-directory',
-    component: ProductDirectoryPage
-  },
-];
+  }
+]
 
 @NgModule({
-  imports: [
-    SharedModule,
-    RouterModule.forChild(routes)],
-  declarations: [
-    ProductPage,
-    ProductDetailPage,
-    ProductDirectoryPage
-  ]
+  imports: [SharedModule, RouterModule.forChild(routes)],
+  declarations: [ProductPage]
 })
-export class ProductPageModule {
-}
+export class ProductPageModule {}
--- src/app/routes/product/product.page.ts
+++ src/app/routes/product/product.page.ts
@@ -1,91 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Component, OnInit } from '@angular/core'
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.page.html',
   styleUrls: ['./product.page.scss']
 })
 export class ProductPage implements OnInit {
-  public colorList = {
-    color1: {
-      icon: '#d5a441',
-      text: '#ffffff',
-      background1: '#d5a441',
-      background2: '#dfac45'
-    },
-    color2: {
-      icon: '#5ea8d6',
-      text: '#ffffff',
-      background1: '#5ea8d6',
-      background2: '#69baec'
-    },
-    color3: {
-      icon: '#9978d6',
-      text: '#ffffff',
-      background1: '#9978d6',
-      background2: '#a984ec'
-    },
-    color4: {
-      icon: '#43af7d',
-      text: '#FFFFF3',
-      background1: '#43af7d',
-      background2: '#49bd88'
-    }
-  };
+  constructor() {}
 
-  public menuList = [
-    {
-      icon: 'menu',
-      label: '产品目录',
-      url: '/product/product-directory'
-    },
-    {
-      icon: 'search',
-      label: '产品查询',
-      url: '',
-    },
-    {
-      icon: 'pricetags',
-      label: '产品对比',
-      url: '',
-    },
-    {
-      icon: 'stats',
-      label: '产品排名',
-      url: '',
-    }
-  ];
-
-  public productList = [
-    {
-      type: 'E时代商户贷款',
-      color: this.colorList.color1,
-      level: '低',
-      content: '针对中小企业的商户贷款，快速放款',
-      amount: '1-15万',
-      num: '150'
-    },
-    {
-      type: 'E时代商户贷款',
-      color: this.colorList.color2,
-      level: '低',
-      content: '针对中小企业的商户贷款，快速放款',
-      amount: '1-15万',
-      num: '150'
-    },
-    {
-      type: 'E时代商户贷款',
-      color: this.colorList.color3,
-      level: '低',
-      content: '针对中小企业的商户贷款，快速放款',
-      amount: '1-15万',
-      num: '150'
-    }
-  ];
-
-  constructor(public router: Router) { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
