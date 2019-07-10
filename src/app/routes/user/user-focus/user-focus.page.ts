38cfe3924431cc09e0487cb759053e91021a69ec
chenxiaoji <15929365823@139.com>
2019-07-10 11:03:28 +0800

    wip(user/*): 新增了我的关注

--- src/app/routes/user/user-focus/user-focus.page.ts
+++ src/app/routes/user/user-focus/user-focus.page.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./user-focus.page.scss']
 })
 export class UserFocusPage implements OnInit {
+  private myFocus = '我的关注'
+  private userFocusTabs = [
+    { tabName: '营销活动', length: 1 },
+    { tabName: '客户', length: 1 },
+    { tabName: '产品', length: 1 },
+    { tabName: '渠道', length: 1 },
+    { tabName: '事件', length: 1 }
+  ]
+  public tabIndex = 0
   constructor() {}
 
   public ngOnInit() {}
+
+  public onTabChange({ index }) {
+    // this.currentMessageType =
+    //   index === 0 ? '' : this.messageTypeNumber[index - 1].code
+  }
 }
