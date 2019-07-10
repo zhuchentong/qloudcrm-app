47414655982ad14ef8c24fba1744512f839142fc
chenaihong <chenaihong@gcstgroup.com>
2019-07-10 21:30:41 +0800

    wip(customer/update2): 客户界面提交

---
+++ src/app/routes/customer/customer-depquery/customer-depquery.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-customer-depquery',
+  templateUrl: './customer-depquery.page.html',
+  styleUrls: ['./customer-depquery.page.scss']
+})
+export class CustomerDepqueryPage implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
